444865fec127925d306c4d0d45f1872a02fd4e5c
Julianne Arroyo <jnarroyo26@gmail.com>
2015-10-31 12:13:05 -0500

    changed branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
      pages: {
        options: {
          remote: 'git@github.com:JulieArroyo/que-pincho.git',
-         branch: 'gh-pages'
+         branch: 'master'
        }
      }
    },
