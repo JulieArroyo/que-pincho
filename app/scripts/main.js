6bdc2aec6377839ff4346cf1aee5ca9ffd5a6827
Julianne Arroyo <jnarroyo26@gmail.com>
2015-12-06 14:12:31 -0600

    deleted extra nonsense

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+//google maps
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
